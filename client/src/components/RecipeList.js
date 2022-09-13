5d87da1bd8864a03516e6c1f585951729a72fe05
marketapolakovau <marketa.polakova@unicorn.com>
2022-09-13 15:40:59 +0200

    moved recipes data into js file

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import RecipeCard from './RecipeCard'
-import Row from 'react-bootstrap/Row';
+import React from "react";
+import RecipeCard from "./RecipeCard";
+import Row from "react-bootstrap/Row";
 
-
-function RecipeList({recipes}) {
+function RecipeList({ recipes }) {
   return (
-    <Row style={{margin:"1rem", backgroundColor:"#f8edeb"}} xs={1} md={3} className="g-4">
-    {recipes.map((recipe)=>{
-        return<RecipeCard key={recipe.id} recipe={recipe} />
-         
-    })}
+    <Row
+      style={{ margin: "1rem", backgroundColor: "#f8edeb" }}
+      xs={1}
+      md={3}
+      className="g-4"
+    >
+      {recipes.map((recipe) => {
+        return <RecipeCard key={recipe.id} recipe={recipe} />;
+      })}
     </Row>
-  )
+  );
 }
 
-export default RecipeList
+export default RecipeList;
